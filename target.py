8956af3807d3366c4aae1cf9c8eb3b464b9b9dd1
iamgokulchowdary <118587663+iamgokulchowdary@users.noreply.github.com>
2023-05-12 09:06:54 +0530

    Update target.py

--- target.py
+++ target.py
@@ -15,4 +15,51 @@ for i in range(len(list)):
     for j in str:
         if list[i] == j:
             count += 1
+    
+str = input("Enter a string : ")
+
+
+
+str = str.split()
+
+
+
+list = []
+
+
+
+for i in str:
+
+    if i in list:
+
+        pass
+
+    else:
+
+        list.append(i)
+
+maxstring = ""
+
+maxcount = 0
+
+for i in range(len(list)):
+
+    count = 0
+
+    for j in str:
+
+        if list[i] == j:
+
+            count += 1
+
     print("Letter : ",list[i]," is repeted : ",count)
+
+    
+
+    if maxcount<count:
+
+        maxcount = count
+
+        maxstring = list[i]
+
+print("The word",maxstring, " is repeated for the maximum of times")
